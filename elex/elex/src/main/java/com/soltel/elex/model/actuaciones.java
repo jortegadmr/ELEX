3b5a607d244a7fb11e42322e336d65586a4645c8
Jao <jaortegadur@gmail.com>
2024-03-20 16:30:33 +0100

    [+]

--- elex/elex/src/main/java/com/soltel/elex/model/actuaciones.java
+++ elex/elex/src/main/java/com/soltel/elex/model/actuaciones.java
@@ -1,6 +1,6 @@
 package com.soltel.elex.model;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
