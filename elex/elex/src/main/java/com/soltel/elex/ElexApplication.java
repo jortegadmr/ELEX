04e217f1fbc540886e0ac9d44854cf96a5b5b36d
Jao <jaortegadur@gmail.com>
2024-03-21 12:01:43 +0100

    [+] Swagger

--- elex/elex/src/main/java/com/soltel/elex/ElexApplication.java
+++ elex/elex/src/main/java/com/soltel/elex/ElexApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElexApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElexApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ElexApplication.class, args);
+    }
 
 }
