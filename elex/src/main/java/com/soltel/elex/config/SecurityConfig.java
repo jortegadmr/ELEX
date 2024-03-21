a818a90c26f9c69d834f408e25df1b82c8134892
Jao <jaortegadur@gmail.com>
2024-03-21 21:11:36 +0100

    [+] Config

---
+++ elex/src/main/java/com/soltel/elex/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.soltel.elex.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filtro(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
+            .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF
+            .authorizeHttpRequests(auth -> auth
+            		.requestMatchers("/clientes/**", "/reservas/**").permitAll() 	// Permitir solicitudes a /clientes y /reservas
+                .anyRequest().authenticated())
+            .formLogin(form -> form
+                .defaultSuccessUrl("/inicio", true));
+                
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder codificador() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService servicio(PasswordEncoder codificador) {
+        UserDetails usuarioAdmin = User.builder()
+            .username("soltel")
+            .password(codificador.encode("admin"))
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(usuarioAdmin);
+    }
+}
