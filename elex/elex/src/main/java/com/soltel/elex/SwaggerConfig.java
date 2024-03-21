19d7adbb103d4dda57c4eeaa38a4808fe0a5ff48
Jao <jaortegadur@gmail.com>
2024-03-21 12:40:45 +0100

    [+] Version sin error aparente

--- elex/elex/src/main/java/com/soltel/elex/SwaggerConfig.java
+++ elex/elex/src/main/java/com/soltel/elex/SwaggerConfig.java
@@ -2,19 +2,19 @@ package com.soltel.elex;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.soltel.elex.controller"))
                 .paths(PathSelectors.any())
