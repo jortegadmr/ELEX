f040552df416f434a5bcad3f90ebee977305f7f6
Jao <jaortegadur@gmail.com>
2024-03-21 09:19:56 +0100

    [+-] Error

--- elex/elex/src/main/java/com/soltel/elex/controller/actuacionesController.java
+++ elex/elex/src/main/java/com/soltel/elex/controller/actuacionesController.java
@@ -1,5 +1,23 @@
 package com.soltel.elex.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.soltel.elex.model.ActuacionesModel;
+import com.soltel.elex.services.ActuacionesService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/actuaciones")
 public class ActuacionesController {
 
+    @Autowired
+    private ActuacionesService actuacionesService;
+    @GetMapping
+    public List<ActuacionesModel> getAllActuaciones() {
+        return this.actuacionesService.getAllActuaciones();
+    }
+    
 }
