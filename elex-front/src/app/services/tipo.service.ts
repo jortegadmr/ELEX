54274492c65ae0568eeeccd25cf3d690ced1db8a
Jao <jaortegadur@gmail.com>
2024-03-23 11:14:59 +0100

    [+] EMPEZAMOS ANGULAR

---
+++ elex-front/src/app/services/tipo.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoService {
+
+  private http = inject(HttpClient);
+
+  list() {
+    return this.http.get('http://localhost:8008/tipo-expediente/consultar');
+  }
+}
---
+++ elex-front/src/app/tipo-expediente/tipo-expediente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-expediente',
+  standalone: true,
+  imports: [],
+  templateUrl: './tipo-expediente.component.html',
+  styleUrl: './tipo-expediente.component.css'
+})
+export default class TipoExpedienteComponent {
+
+}
