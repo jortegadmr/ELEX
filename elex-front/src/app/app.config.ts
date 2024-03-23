148cd8a895a23c3be7543b33162028deaa2a2855
Jao <jaortegadur@gmail.com>
2024-03-23 10:22:55 +0100

    [+]

--- elex-front/src/app/app.config.ts
+++ elex-front/src/app/app.config.ts
@@ -4,5 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    
+  ]
 };
