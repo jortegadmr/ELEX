611317eb6d4cd14b8f5d95fe8be86fd319fab3a8
Jao <jaortegadur@gmail.com>
2024-03-26 08:42:27 +0100

    [+] Menu

--- elex-front/src/app/app.routes.ts
+++ elex-front/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
+        loadComponent: () => import('./home/home.component')
+    },
+    {
+        path: 'expedientes',
         loadComponent: () => import('./tipo-expediente/tipo-expediente.component')
     }
+    
 ];
