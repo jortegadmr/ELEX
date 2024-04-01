71fc03956d4a93aa870efb65a29aea8a6620c922
Jao <jaortegadur@gmail.com>
2024-04-01 11:38:31 +0200

    [+-]

--- elex-front/src/app/app.module.ts
+++ elex-front/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
-import { RoutingModule } from './app.routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-    RoutingModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule
+
   ],
   exports: [
     RouterModule
--- elex-front/src/app/tipo-expediente/tipo-expediente.component.ts
+++ elex-front/src/app/tipo-expediente/tipo-expediente.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TipoService } from '../services/tipo.service';
+
+
 @Component({
   selector: 'app-tipo-expediente',
   standalone: true,
@@ -7,12 +10,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './tipo-expediente.component.html',
   styleUrl: './tipo-expediente.component.css'
 })
-export default class TipoExpedienteComponent {
+
+
+export default class TipoExpedienteComponent implements OnInit {
+
 // Método para alternar la visibilidad del menú
-toggleMenu() {
-  const ocultarElements = document.querySelectorAll('.ocultar');
-  ocultarElements.forEach(element => {
-    element.classList.toggle('active');
-  });
-}
+  toggleMenu() {
+    const ocultarElements = document.querySelectorAll('.ocultar');
+    ocultarElements.forEach(element => {
+      element.classList.toggle('active');
+    });
+  }
+
+    private contactService = inject(TipoService);
+
+    ngOnInit(): void {
+        this.tipoService = this.contactService
+    }
+    }
+
 }
