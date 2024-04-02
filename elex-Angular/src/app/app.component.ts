16733544f07d8199abffe775f47dae5e36ddd4db
Jao <jaortegadur@gmail.com>
2024-04-02 10:55:07 +0200

    [INICIO ANGULAR ]

--- elex-Angular/src/app/app.component.ts
+++ elex-Angular/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+
+  /* importar los modulos que vayamos a utilizar  */
+
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- elex-Angular/src/app/shared/header/header.component.ts
+++ elex-Angular/src/app/shared/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+
+  /* importar los modulos que vayamos a utilizar  */
+
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
+
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
