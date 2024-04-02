2d18c979f6967e25507b6c68bc02e72eca859404
Jao <jaortegadur@gmail.com>
2024-04-02 18:34:47 +0200

    [+] LOGIN PROLIJO

--- elex-Angular/src/app/auth/login/login.component.ts
+++ elex-Angular/src/app/auth/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
 
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink,
+    RouterOutlet,
+    CommonModule
   ],
 
   templateUrl: './login.component.html',
@@ -14,22 +20,30 @@ import { FormBuilder, FormControl, ReactiveFormsModule, FormGroup, Validators } 
 })
 
 export class LoginComponent {
-  /* loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  }); */
+
+  ngOnInit(): void {}
+  
   loginForm = this.formBuilder.group({
     username: ['', Validators.required],
     password: ['', Validators.required]
   });
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
+  get username(){
+    return this.loginForm.get('username');
+  }
+  get password(){
+    return this.loginForm.get('password');
+  }
   login(){
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this.router.navigate(['inicio']);
+      this.loginForm.reset();
     }
     else {
-      alert('Error con los datos');
+      this.loginForm.markAllAsTouched();
+      alert('Usuario o contraseña incorrectos');
     }
   }
 }
