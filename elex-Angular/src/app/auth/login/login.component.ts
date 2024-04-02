5f3160c426aed6365c6fdb1908861ecd1a4f0ce0
Jao <jaortegadur@gmail.com>
2024-04-02 17:40:28 +0200

    [+] FormBuilder

--- elex-Angular/src/app/auth/login/login.component.ts
+++ elex-Angular/src/app/auth/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+
+  imports: [
+    ReactiveFormsModule
+  ],
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
